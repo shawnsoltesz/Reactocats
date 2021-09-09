4de9f7f152af8f9313b97b65fe69942d2570ee8c
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-09 14:21:25 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,7 @@ export function App() {
 
 //<!DOCTYPE html>
 ;<html lang="en">
-  <head>
-    {/* <meta charset="UTF-8" /> */}
-    <link rel="icon" type="image/svg+xml" href="favicon.svg" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Octodex</title>
-  </head>
+  <head></head>
   <body>
     <header>
       <ul className="left">
