0f8e8909b5571772e6c6d1b4889935b201b2efa4
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-09 21:37:28 -0400

    cat #153 converted to props

--- src/components/Cat.tsx
+++ src/components/Cat.tsx
@@ -2,31 +2,27 @@ import React from 'react'
 
 export function Cat(props) {
   return (
-    <section className="octocat">
-      <ul className="cats">
-        <li className="octodex-cat">
-          <a className="cat" href={props.image}>
-            <img src={props.source} alt={props.name} />
+    <li className="octodex-cat">
+      <a className="cat" href={props.image}>
+        <img src={props.source} alt={props.name} />
+      </a>
+      <div className="image-footer">
+        <div className="number-name">
+          <p className="number">#{props.number} &nbsp;:</p>
+          <a className="name" href={props.image}>
+            {props.name}
           </a>
-          <div className="image-footer">
-            <div className="number-name">
-              <p className="number">#153: &nbsp;</p>
-              <a className="name" href={props.image}>
-                {props.name}
-              </a>
-            </div>
-            <div className="artist-image">
-              <a className="artist" href={props.artistProfile}>
-                <img
-                  className="image"
-                  src={props.artistImage}
-                  alt={props.artistImageAlt}
-                />
-              </a>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </section>
+        </div>
+        <div className="artist-image">
+          <a className="artist" href={props.artistProfile}>
+            <img
+              className="image"
+              src={props.artistImage}
+              alt={props.artistImageAlt}
+            />
+          </a>
+        </div>
+      </div>
+    </li>
   )
 }
